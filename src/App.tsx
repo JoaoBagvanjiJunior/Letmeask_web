58f8cc9af76c5732d885f1d4aed275e6d604a235
JoaoBagvanji <jbagvanji54@gmail.com>
2024-10-24 15:29:24 -0300

    Letmeask commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Button} from './Components/Button';
+
+
+
+
+function App() {
+  return (
+    <div>
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+    </div>
+  );
+}
+
+export default App;
